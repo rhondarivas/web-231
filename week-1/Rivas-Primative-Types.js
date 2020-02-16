d598a66cd8c07c9cdf9a07405edb1aad38b98a58
Rhonda Rivas <verano@Rhondas-Air.attlocal.net>
2020-02-16 18:49:23 -0500

    Rivas-Types-Values-Variables.js

---
+++ week-1/Rivas-Primative-Types.js
@@ -0,0 +1,29 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Professor Krasso
+; Date: 16 Feb 2020
+; Modified By: Rhonda Rivas
+; Description: This program demonstrates the
+; use of JavaScript
+; variables in an application.
+;===========================================
+*/ 
+var firstName = "Rhonda";
+
+console.log(firstName);
+
+var lastName = "Rivas";
+console.log(lastName);
+
+var age = "43";
+console.log(age);
+
+var street = "Some random street address";
+console.log(street);
+
+var city = "some random city name";
+console.log(city);
+
+var zipCode = "some random zipcode";
+console.log(zipCode);
