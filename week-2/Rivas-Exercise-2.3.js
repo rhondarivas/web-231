8e86957ee6535bd6a60206bfdff5790f8ae44d65
Rhonda Rivas <verano@Rhondas-Air.attlocal.net>
2020-02-24 15:21:03 -0500

    Adding Rivas-Exercise-2.3.js

---
+++ week-2/Rivas-Exercise-2.3.js
@@ -0,0 +1,27 @@
+var header = required('..header.js');
+
+//start program
+
+/*
+    Expected output:
+
+    Rhonda Rivas
+    Exercise 2.3
+    24 February 2020
+
+    Rhonda Rivas
+    */
+    myName.rhonda ="Rhonda";
+
+    //function
+    function myName() {
+        return myName.rhonda
+    }
+
+    //output
+    console.log('/n');
+    console.log(myName());
+
+
+
+    //end program
