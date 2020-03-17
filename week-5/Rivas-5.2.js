29d05181489df20f6b96126902982db4efc9d5cb
Rhonda Rivas <verano@Rhondas-Air.attlocal.net>
2020-03-16 20:51:43 -0400

    Rivas-5.2.js

---
+++ week-5/Rivas-5.2.js
@@ -0,0 +1,24 @@
+/*============================================
+; Title: Assignment 5.2
+; Author: Richard Krasso
+; Date: 16 March 2020
+; Modified By: Rhonda Rivas
+; Description: This program demonstrates the
+; use of JavaScript Built In Functions
+===========================================
+*/
+
+/*
+Expected Output:
+
+Pizza
+Chipotle
+Chicken Spinach Salad
+Lasagna
+Paella
+
+*/
+var faveFoods =["Pizza", "Chipotle", "Chicken Spinach Salad", "Lasagna", "Paella"];
+
+faveFoods.forEach(function(faveFoods)
+{console.log(faveFoods)});
